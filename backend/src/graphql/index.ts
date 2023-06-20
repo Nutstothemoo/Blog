2f73d57a99808c44c33f870439f47d4ea45170fd
DUPIN <maximedupin1992@gmail.com>
2023-06-20 14:42:03 +0200

    fix compileroptions

--- backend/src/graphql/index.ts
+++ backend/src/graphql/index.ts
@@ -1,10 +1,9 @@
 // import * as articles from "./articles";
 import * as articles from './articles'
 
-const types = [articles.types];
+const types = [];
 const queries = [];
 const mutations = [];
 
-const entities = [articles];
 
-export const typeDefs = `${types.join("\n")}`;
+export const typeDefs = ``;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,54 +3,9 @@ import {
   startServerAndCreateLambdaHandler,
   handlers,
 } from "@as-integrations/aws-lambda";
-// import resolvers from "./resolvers/resolvers";
-// import {typeDefs} from "./graphql/index";
+import resolvers from "./resolvers";
+import typeDefs from "./types";
 
-const typeDefs = `#graphql
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    posts: [Post!]!
-  }
-
-  type Category {
-    id: ID!
-    name: String!
-    description: String!
-  }
-
-  type Post {
-    id: ID!
-    title: String!
-    content: String!
-    category: Category!
-    author: User!
-    created_at: String!
-  }
-
-  type Query {
-    hello: String
-    users: [User!]!
-    postsByCategory(categoryId: ID!): [Post!]!
-    postsByAuthor(authorId: ID!): [Post!]!
-  }
-
-  type Mutation {
-    createPost(
-      title: String!
-      content: String!
-      categoryId: ID!
-      authorId: ID!
-    ): Post!
-  }
-`;
-// console.log(resolvers);
-const resolvers = {
-  Query: {
-    hello: () => "world",
-  },
-};
 const server = new ApolloServer({
   typeDefs,
   resolvers: resolvers,
